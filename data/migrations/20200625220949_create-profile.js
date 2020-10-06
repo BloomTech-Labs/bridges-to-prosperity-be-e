3a2cc3ea6aa39261e41eff0bf99b243e1fcda379
meloegel <meloegel@gmail.com>
2020-10-06 18:59:03 -0400

    removed un-needed comments

--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -11,39 +11,39 @@ exports.up = (knex) => {
 
     .createTable('bridges', function (bridges) {
       bridges.increments();
-      bridges.string('country'); //
-      bridges.integer('district_id');//
-      bridges.string('province');//
-      bridges.string('district');//
-      bridges.string('sector');//
-      bridges.string('sector_id');//
-      bridges.string('cell');//
-      bridges.string('cell_id');//
-      bridges.string('village');//
-      bridges.string('village_id');//
-      bridges.string('name'); //
-      bridges.string('stage');//
-      bridges.string('sub_stage'); //
-      bridges.string('project_code').unique();//
-      bridges.string('type'); //
-      bridges.string('span');//
-      bridges.float('Lat');//
-      bridges.float('Long');//
-      bridges.string('Individuals_directly_served'); //
-      bridges.string('form');//
-      bridges.string('case_safe_id');//
-      bridges.string('opportunity_id');//
+      bridges.string('country');
+      bridges.integer('district_id');
+      bridges.string('province');
+      bridges.string('district');
+      bridges.string('sector');
+      bridges.string('sector_id');
+      bridges.string('cell');
+      bridges.string('cell_id');
+      bridges.string('village');
+      bridges.string('village_id');
+      bridges.string('name');
+      bridges.string('stage');
+      bridges.string('sub_stage');
+      bridges.string('project_code').unique();
+      bridges.string('type');
+      bridges.string('span');
+      bridges.float('Lat');
+      bridges.float('Long');
+      bridges.string('Individuals_directly_served');
+      bridges.string('form');
+      bridges.string('case_safe_id');
+      bridges.string('opportunity_id');
       bridges.string('bridge_image');
-      bridges.string('community_served_1'); //
-      bridges.string('community_served_1_id'); //
-      bridges.string('community_served_2');//
-      bridges.string('community_served_2_id');//
+      bridges.string('community_served_1');
+      bridges.string('community_served_1_id');
+      bridges.string('community_served_2');
+      bridges.string('community_served_2_id');
       bridges.string('community_served_3');
-      bridges.string('community_served_3_id');//
-      bridges.string('community_served_4');//
-      bridges.string('community_served_4_id');//
-      bridges.string('community_served_5');//
-      bridges.string('community_served_5_id');//
+      bridges.string('community_served_3_id');
+      bridges.string('community_served_4');
+      bridges.string('community_served_4_id');
+      bridges.string('community_served_5');
+      bridges.string('community_served_5_id');
     });
 };
 
@@ -51,41 +51,3 @@ exports.down = async (knex) => {
   await knex.schema.dropTableIfExists('bridges');
   return knex.schema.dropTableIfExists('profiles');
 };
-
-
-// country//
-// province//
-// district//
-// district_id//
-// sector//
-// sector_id//
-// cell//
-// cell_id//
-// village//
-// village_id//
-// project_code//
-// sub_stage //
-// Individuals_directly_served //
-// span//
-// lat //
-// long //
-// type //
-// name//
-// stage//
-// form //
-// case_safe_id//
-// opportunity_id//
-// community_served_1 //
-// community_served_1_id//
-// community_served_2//
-// community_served_2_id//
-// community_served_3//
-// community_served_3_id//
-// community_served_4//
-// community_served_4_id//
-// community_served_5//
-// community_served_5_id//
-
-
-// old:
-// bridges.specificType('communities_served', 'text ARRAY');
